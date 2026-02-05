ae218c32393ef894fc10e080567006257fbbfc0e
Harish Sahu <harish12121999@gmail.com>
2026-02-05 15:48:43 +0530

    create address api getAddressByUserId

--- src/main/java/com/e_commerce_backend/e_commerce_backend/controller/AddressController.java
+++ src/main/java/com/e_commerce_backend/e_commerce_backend/controller/AddressController.java
@@ -2,13 +2,11 @@ package com.e_commerce_backend.e_commerce_backend.controller;
 
 import com.e_commerce_backend.e_commerce_backend.Utility.helperClass.AuthUtil;
 import com.e_commerce_backend.e_commerce_backend.entity.Dto.AddressDTO;
+import com.e_commerce_backend.e_commerce_backend.entity.dtoResponse.AddressResponse;
 import com.e_commerce_backend.e_commerce_backend.serviceImp.AddressServiceImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -28,4 +26,13 @@ public class AddressController {
         AddressDTO userAddress = addressService.addUserAddress(addressDTO, userId);
         return new ResponseEntity<>(userAddress , HttpStatus.FOUND);
     }
+
+//   http://localhost:8081/api/v1/user/{user_id}/get-address
+    @GetMapping("/user/{user_id}/get-address")
+    public ResponseEntity<AddressResponse> getUserAddress(@PathVariable Long user_id ){
+        AddressResponse userAddress = addressService.getUserAddress(user_id);
+        return new ResponseEntity<>(userAddress,  HttpStatus.FOUND);
+    }
+
+
 }
