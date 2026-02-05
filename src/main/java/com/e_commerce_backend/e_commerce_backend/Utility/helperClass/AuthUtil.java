b453bafc4cf92b74440bf2cad62583a4cc3c7f5a
Harish Sahu <harish12121999@gmail.com>
2026-02-05 14:45:43 +0530

    Add logs into AuthUtil for debugging

--- src/main/java/com/e_commerce_backend/e_commerce_backend/Utility/helperClass/AuthUtil.java
+++ src/main/java/com/e_commerce_backend/e_commerce_backend/Utility/helperClass/AuthUtil.java
@@ -2,6 +2,8 @@ package com.e_commerce_backend.e_commerce_backend.Utility.helperClass;
 
 import com.e_commerce_backend.e_commerce_backend.entity.UserEntity;
 import com.e_commerce_backend.e_commerce_backend.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -9,6 +11,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class AuthUtil {
+
+    private final Logger logger= LoggerFactory.getLogger(AuthUtil.class);
 
     private final UserRepository userRepository;
 
@@ -18,15 +22,19 @@ public class AuthUtil {
 
     public String loggedInEmail(){
         Authentication authentication= SecurityContextHolder.getContext().getAuthentication();
+        logger.info("authentication: " +authentication);
         UserEntity user = userRepository.findByUsername(authentication.getName()).orElseThrow(
                 () -> new UsernameNotFoundException("username not found"));
+        logger.info("user:" +user);
         return user.getEmail();
     }
 
-    public Long loggedInUserId(){
-        Authentication authentication=SecurityContextHolder.getContext().getAuthentication();
+    public Long loggedInUserId() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        logger.info("authentication: " +authentication);
         UserEntity user = userRepository.findByUsername(authentication.getName())
                 .orElseThrow(() -> new UsernameNotFoundException("username not found"));
+        logger.info("user:" +user.getUsername());
         return user.getId();
     }
 }
