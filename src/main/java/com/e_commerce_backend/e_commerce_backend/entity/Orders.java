5204722df641ec392c9981afe51a15caa4e52da1
Harish Sahu <harish12121999@gmail.com>
2026-02-06 17:14:39 +0530

    create-order-API

--- src/main/java/com/e_commerce_backend/e_commerce_backend/entity/Orders.java
+++ src/main/java/com/e_commerce_backend/e_commerce_backend/entity/Orders.java
@@ -1,70 +1,55 @@
 package com.e_commerce_backend.e_commerce_backend.entity;
 
+import com.e_commerce_backend.e_commerce_backend.entity.Enum.OrderStatus;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.annotation.CreatedDate;
 
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
-@Table
+@Table(name = "orders")
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
 public class Orders {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long Order_id;
 
-    private String email;
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
 
-    private String order_status;
+        private String orderNumber;
 
-    private Double total_amount;
+        @ManyToOne
+        @JoinColumn(name = "user_id")
+        @JsonIgnore
+        private UserEntity user;
 
-//
-//    private CreatedDate createAt;
-//
-//    @Temporal(TemporalType.DATE)
-//    private UpdateTimestamp updateAt;
+        private Double totalAmount;
 
-    public Long getOrder_id() {
-        return Order_id;
+        @Enumerated(EnumType.STRING)
+        private OrderStatus orderStatus;
+
+        @ManyToOne(fetch = FetchType.LAZY)
+        @JoinColumn(name = "address_id")
+        @JsonIgnore
+        private Address address;
+
+        private String paymentStatus;
+
+        private Instant createdAt;
+        private Instant updatedAt;
+
+        @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+        private List<OrderItem> orderItems;
+
     }
 
-    public void setOrder_id(Long order_id) {
-        Order_id = order_id;
-    }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getOrder_status() {
-        return order_status;
-    }
-
-    public void setOrder_status(String order_status) {
-        this.order_status = order_status;
-    }
-
-    public Double getTotal_amount() {
-        return total_amount;
-    }
-
-    public void setTotal_amount(Double total_amount) {
-        this.total_amount = total_amount;
-    }
-
-}
